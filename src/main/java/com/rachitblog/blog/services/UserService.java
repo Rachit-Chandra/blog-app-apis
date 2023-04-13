6c96176f052a29d0bbb45877b9da0fa2c66d079d
Rachit-Chandra <rachitchandra065@gmail.com>
2023-04-13 16:25:04 +0530

    Adding User DTO and Implementing createUser fuctionality

--- src/main/java/com/rachitblog/blog/services/UserService.java
+++ src/main/java/com/rachitblog/blog/services/UserService.java
@@ -1,6 +1,23 @@
 package com.rachitblog.blog.services;
 
+import java.util.List;
+
+import com.rachitblog.blog.payloads.UserDto;
+
 public interface UserService {
 //DEMO TEST PUSH VIA ECLIPSE
 	//Once again
+	
+//	createUser(User user);
+	
+	UserDto createUser(UserDto userDto);
+	
+	UserDto updateUser(UserDto user,Integer userId);
+	
+	UserDto getUserById(Integer userId);
+	
+	List<UserDto> getAllUsers();
+	
+	void deleteUser(Integer userId);
+	
 }
